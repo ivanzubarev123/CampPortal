f1cd8a8ed1f65172be4ef35c75b0285064578262
ivanzubarev123 <62092598+ivanzubarev123@users.noreply.github.com>
2026-05-23 00:55:59 +0300

    Update main.py

--- CampPortal/app/main.py
+++ CampPortal/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.routes import routine
+from app.seed import seed
 
 from app.database import engine, Base
 from app.routes import (
